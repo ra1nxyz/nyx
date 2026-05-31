b98eec4388231f8746657abe1b78edc95cb807f1
ra1nxyz <ov3rsights@protonmail.com>
2026-05-31 02:56:37 +0100

    not how the dispatch works, im annoyed by this

--- update.sh
+++ update.sh
@@ -18,7 +18,4 @@ PACKAGE_VERSION=$(grep '^version *= *' Cargo.toml | head -n1 | cut -d'"' -f2)
 PACKAGE_PATH=$(pwd)
 WARNING_COUNT=$(grep -c '^warning:' "$LOG" || true)
 
-echo "Compiled $PACKAGE_NAME $PACKAGE_VERSION $PACKAGE_PATH | Compile time ${COMPILE_TIME}s | Warnings caught $WARNING_COUNT | Restarting systemd service for instance $SERVICE"
-
-sudo systemctl restart "$SERVICE"
-rm -f "$LOG"
+echo "Compiled $PACKAGE_NAME $PACKAGE_VERSION $PACKAGE_PATH \n Compile time ${COMPILE_TIME}s \n Warnings caught $WARNING_COUNT \n Please restart systemd service for instance $SERVICE"
